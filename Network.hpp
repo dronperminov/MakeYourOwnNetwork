40dcb2bb8f86c88b4a9f54648cbd31c72089bb13
dronperminov <dronperminov@gmail.com>
2020-03-24 00:16:39 +0300

    add training with example

--- Network.hpp
+++ Network.hpp
@@ -6,17 +6,27 @@
 
 using namespace std;
 
+struct NetworkData {
+	vector<vector<double>> x;
+	vector<vector<double>> y;
+};
+
 class Network {
 	int inputs; // число входов
 	int outputs; // число выходов
 	int last; // индекс последнего слоя
 	vector<Layer> layers; // слои
 
+	void Backward(const vector<double> &x, const vector<double> &dout); // обратное распространение ошибки
+	double CalculateLoss(const vector<double> &y, const vector<double> &t, vector<double> &dout); // вычисление ошибки
+	void UpdateWeights(double learningRate); // обновление весовых коэффициентов
+
 public:
 	Network(int inputs); // создание сети
 	
 	void AddLayer(int outputs, const string& function); // добавление слоя
 	void Print() const; // вывод коэффициентов
+	void Train(const NetworkData &data, double learningRate, int epochs, int log_period); // обучение
 	
 	vector<double> Forward(const vector<double> &x); // прямое распространение
 };
@@ -26,6 +36,40 @@ Network::Network(int inputs) {
 	this->inputs = inputs; // запоминаем число входов
 	this->outputs = inputs; // нет слоёв
 	this->last = -1; // ещё нет слоёв
+}
+
+// обратное распространение ошибки
+void Network::Backward(const vector<double> &x, const vector<double> &dout) {
+	if (last == 0) {
+		layers[last].Backward(x, dout);
+		return;
+	}
+
+	layers[last].Backward(layers[last - 1].GetOutput(), dout); // обрабатываем последний слой
+
+	for (int i = last - 1; i >= 1; i--)
+		layers[i].Backward(layers[i - 1].GetOutput(), layers[i + 1].GetDx()); // обрабатываем промежуточные слои
+
+	layers[0].Backward(x, layers[1].GetDx()); // обрабатываем первый слой
+}
+
+// вычисление ошибки
+double Network::CalculateLoss(const vector<double> &y, const vector<double> &t, vector<double> &dout) {
+	double loss = 0;
+
+	for (int i = 0; i < outputs; i++) {
+		double e = y[i] - t[i]; // находим разность между элементами
+		dout[i] = 2 * e; // записываем производную функции ошибки
+		loss += e * e; // добавляем ошибку
+	}
+
+	return loss; // возвращаем ошибку
+}
+
+// обновление весовых коэффициентов
+void Network::UpdateWeights(double learningRate) {
+	for (int i = 0; i < layers.size(); i++)
+		layers[i].UpdateWeights(learningRate); // обновляем веса у каждого из слоёв
 }
 
 // добавление слоя
@@ -40,6 +84,26 @@ void Network::Print() const {
 	for (int i = 0; i < layers.size(); i++) {
 		cout << "layer " << i << ": " << endl;
 		layers[i].PrintWeights();
+	}
+}
+
+// обучение сети
+void Network::Train(const NetworkData& data, double learningRate, int epochs, int log_period) {
+
+	for (int epoch = 0; epoch < epochs; epoch++) {
+		double loss = 0;
+
+		for (int i = 0; i < data.x.size(); i++) {
+			vector<double> out = Forward(data.x[i]); // выполняем прямое распространение
+			vector<double> dout(outputs); // создаём вектор производных функции потерь
+			loss += CalculateLoss(out, data.y[i], dout); // вычисляем функцию потерь
+			Backward(data.x[i], dout); // выполняем обратное распространение
+			UpdateWeights(learningRate); // обновляем весовые коэффициенты
+		}
+
+		// если нужно
+		if (epoch % log_period == 0 || epoch == epochs - 1)
+			cout << "Epoch: " << epoch << ", loss: " << loss << endl; // выводим текущую эпоху и ошибку
 	}
 }
 
--- main.cpp
+++ main.cpp
@@ -1,17 +1,51 @@
 #include <iostream>
 #include "Network.hpp"
 
+using namespace std;
+
+// вывод вектора
+void PrintVector(const vector<double> &v) {
+	cout << "[ ";
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i] << " ";
+	cout << "]";
+}
+
 int main() {
-	Network network(2);
-	network.AddLayer(5, "sigmoid");
-	network.AddLayer(3, "tanh");
+	NetworkData trainData; // задаём данные для XOR проблемы
+	trainData.x = {
+		{ 0, 0 },
+		{ 0, 1 },
+		{ 1, 0 },
+		{ 1, 1 },
+	};
 
-	vector<double> x = { 0.5, -0.3 };
-	vector<double> y = network.Forward(x);
+	trainData.y = {
+		{ 0 },
+		{ 1 },
+		{ 1 },
+		{ 0 },
+	};
 
-	cout << "x: " << x[0] << " " << x[1] << endl;
-	cout << "y: " << y[0] << " " << y[1] << " " << y[2] << endl;
+	double learningRate = 0.5; // скорость обучения
+	int epochs = 10000; // число эпох
 
-	cout << "Network weights: " << endl;
-	network.Print();
+	Network network(2); // создаём сеть из двух входов
+	network.AddLayer(5, "tanh"); // добавляем слой из 5 нейронов
+	network.AddLayer(1, "sigmoid"); // добавляем слой из одного нейрона
+
+	network.Train(trainData, learningRate, epochs, 1000); // обучаем
+
+	// выводим результат обучения
+	for (int i = 0; i < trainData.x.size(); i++) {
+		vector<double> y = network.Forward(trainData.x[i]);
+
+		cout << "x: ";
+		PrintVector(trainData.x[i]);
+		cout << "\tcorrect:";
+		PrintVector(trainData.y[i]);
+		cout << "\tnetwork: ";
+		PrintVector(y);
+		cout << endl;
+	}
 }
