ca0277d75995558381b807f2721681326972d358
dronperminov <dronperminov@gmail.com>
2020-03-24 14:22:28 +0300

    add mnist classifier example

---
+++ examples/mnist_classifier.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+#include "../Network.hpp"
+#include "../utils/DataLoader.hpp"
+
+int Argmax(const vector<double> &x) {
+	int imax = 0;
+
+	for (int i = 1; i < x.size(); i++)
+		if (x[i] > x[imax])
+			imax = i;
+
+	return imax;
+}
+
+double Test(Network &network, const NetworkData& data) {
+	int correct = 0;
+	double total = data.x.size();
+
+	for (int i = 0; i < data.x.size(); i++) {
+		vector<double> y = network.Forward(data.x[i]);
+
+		int imax1 = Argmax(y);
+		int imax2 = Argmax(data.y[i]);
+
+		correct += imax1 == imax2;
+	}
+	
+	return correct / total;
+}
+
+int main() {
+	DataLoader loader("../datasets/mnist.txt");
+	NetworkData trainData = loader.LoadData("../datasets/mnist_train.csv");
+	NetworkData testData = loader.LoadData("../datasets/mnist_test.csv");
+
+	Network network(784);
+	network.AddLayer("fc 100");
+	network.AddLayer("activation sigmoid");
+	network.AddLayer("fc 10");
+	network.AddLayer("activation sigmoid");
+
+	double learningRate = 0.08;
+	int epochs = 20;
+	int testPeriod = 5;
+
+	cout << "Init accuracy: " << Test(network, testData) << endl;
+
+	for (int i = 0; i < epochs / testPeriod; i++) {
+		network.Train(trainData, learningRate, testPeriod, 1);
+		
+		cout << "Train accuracy: " << Test(network, trainData) << endl;
+		cout << " Test accuracy: " << Test(network, testData) << endl;
+	}
+}
